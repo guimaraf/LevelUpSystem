ef9f98482bbf5db2041221dfd8e1a22525cc9e43
guimaraf <guimaraf@gmail.com>
2019-07-25 10:04:53 -0300

    first version

---
+++ Assets/scripts/Level.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private Text txtCurrentLevel = default, txtCurrentXp = default, txtNextXP = default;
+    [SerializeField] private int currentLevel = 1; 
+    [SerializeField] private float currentXp = 0, nextXp = 50;
+
+    private void Start()
+    {
+        UpdateUi();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetXp(10);
+        }
+    }
+
+    private void SetXp(int xp)
+    {
+        currentXp += xp;
+        if(currentXp >= nextXp)
+        {
+            nextXp = nextXp * 1.25f;
+            currentLevel++;
+            currentXp = 0f;
+        }
+        UpdateUi();
+    }
+
+    private void UpdateUi()
+    {
+        txtCurrentLevel.text = currentLevel.ToString();
+        txtCurrentXp.text = currentXp.ToString();
+        txtNextXP.text = nextXp.ToString();
+    }
+
+    
+}
